23890388e3556b56642fb29afe52f15959edbeb6
admin <lh0940871@gmail.com>
2019-11-28 13:33:50 -0600

    inciando hotel

---
+++ index.php
@@ -0,0 +1,20 @@
+<DOCTORTYPE.html>
+<meta charset="uft-8">    
+<html lang="es">
+      <head>
+      <title>hotel</title> 
+    </head>
+    
+      <body>
+        <header>
+          <div class="contenedor">
+         <nav class="menu">
+            <li><a href="acerca_de.html" id="btn-acerca-de">acerca de</a></li>
+             <li><a href="galeria.html" id="btn-menu">galeria</a></li>
+             <li><a href="informacion.html" id="btn-galeria">informacion</a></li>
+             <img src="img/logo.jpg" alt="">
+              </nav>
+            </div>
+      </header>  
+    </body>
+</html>
